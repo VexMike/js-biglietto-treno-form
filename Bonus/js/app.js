9bbccc3ae65cdff2be0142b26374526299f01d7c
Urbani Michele <michele.urbani094@gmail.com>
2024-03-13 19:55:04 +0100

    ADD: Fix Type button --> submit, Fix event form, select value e sconto

--- Bonus/js/app.js
+++ Bonus/js/app.js
@@ -15,9 +15,7 @@ const formElement = document.getElementById("form-ticket");
 // €/km
 const prezzoPerKm = 0.21; // number
 
-// Calcolare il prezzo al km
-const prezzoBase = km * prezzoPerKm; // Number
-console.log(prezzoBase, typeof prezzoBase);
+
 
 // Prendere il button per impostarlo come input button
 const buttonElement = document.getElementById("submit");
@@ -25,13 +23,9 @@ console.log(buttonElement);
 
 
 
-// Validazione dati
-// km > 0
-console.log(km > 0);
-// km non è un NaN
-console.log(!isNaN(km));
 
-formElement.addEventListener("submit", function () {
+
+formElement.addEventListener("submit", function (event) {
   // Impedisco che il Form venga inviato per poter visualizzare i dati raccolti
   event.preventDefault();
 
@@ -39,15 +33,25 @@ formElement.addEventListener("submit", function () {
   const km = parseFloat(kmInputElement.value /* String */); // Number
   console.log("Km percorsi: ", km);
 
+    // Validazione dati
+    // km > 0
+    console.log(km > 0);
+    // km non è un NaN
+    console.log(!isNaN(km));
+
   const selectValue = selectElement.value;
+  console.log(selectValue)
 
   if (km > 0 && !isNaN(km)) {
 
+    // Calcolare il prezzo al km
+    const prezzoBase = km * prezzoPerKm; // Number
+    console.log(prezzoBase, typeof prezzoBase);
 
-    if (selectValue === "1") {
+    if (selectValue === "2") {
       sconto = prezzoBase * 0.2;
       showDiscout = 20;
-    } else if (selectValue === "2") {
+    } else if (selectValue === "3") {
       // -- ALTRIMENTI SE età passeggero >65 = sconto40%
       sconto = prezzoBase * 0.4;
       showDiscout = 40;
@@ -58,7 +62,7 @@ formElement.addEventListener("submit", function () {
     }
 
     // Prezzo finale
-    const prezzo = prezzoBase - selectValue; // numebr 
+    const prezzo = prezzoBase - sconto; // numebr 
     console.log(prezzo.toFixed(2), typeof prezzo, prezzo)
 
 
@@ -67,13 +71,13 @@ formElement.addEventListener("submit", function () {
     kmElement.innerHTML = km + "km";
 
     const prezzoBaseElement = document.getElementById("prezzo-base");
-    prezzoBaseElement.innerHTML = prezzoBase.toFixed(2) + "&euro";
+    prezzoBaseElement.innerHTML = prezzoBase.toFixed(2) + "€";
 
     const scontoElement = document.getElementById("sconto-applicato");
-    scontoElement.innerHTML = selectValue + "%";
+    scontoElement.innerHTML = showDiscout + "%";
 
     const prezzofinaleElement = document.getElementById("prezzo-totale");
-    prezzofinaleElement.innerHTML = prezzo.toFixed(2) + "&euro";
+    prezzofinaleElement.innerHTML = prezzo.toFixed(2) + "€";
 
     
 
